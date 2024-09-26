f26d5c11f7468290bbc4fb265c76148451cd6b0a
Alpha <121655660+KARTHIK-004@users.noreply.github.com>
2024-09-26 10:56:48 +0530

    Request Leave Updated

--- app/(tabs)/LeaveRequest.jsx
+++ app/(tabs)/LeaveRequest.jsx
@@ -5,19 +5,31 @@ import {
   TextInput,
   TouchableOpacity,
   ScrollView,
+  SafeAreaView,
+  Alert,
+  Linking,
   Image,
 } from "react-native";
-import { images } from "../../constants/images";
-import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
-import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 import { Picker } from "@react-native-picker/picker";
+import { useNavigation } from "@react-navigation/native";
+import { images } from "../../constants/images";
 
 const LeaveRequest = () => {
   const navigation = useNavigation();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [leaveType, setLeaveType] = useState("Vacation");
+  const [formData, setFormData] = useState({
+    name: "",
+    empId: "",
+    email: "",
+    phoneNumber: "",
+    managerName: "",
+    managerEmail: "",
+    leaveType: "",
+    reason: "",
+  });
 
   const handleStartDateChange = (event, selectedDate) => {
     if (selectedDate) setStartDate(selectedDate);
@@ -43,124 +55,248 @@ const LeaveRequest = () => {
     });
   };
 
+  const sendEmail = () => {
+    if (!formData.managerEmail) {
+      Alert.alert("Error", "Please enter the manager's email.");
+      return;
+    }
+
+    const subject = `Leave Request from ${formData.name}`;
+    const body = `
+      Employee ID: ${formData.empId}
+      Phone: ${formData.phoneNumber}
+      Leave Type: ${formData.leaveType}
+      Start Date: ${startDate.toLocaleDateString()}
+      End Date: ${endDate.toLocaleDateString()}
+      Reason: ${formData.reason}
+    `;
+
+    const mailUrl = `mailto:${
+      formData.managerEmail
+    }?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    Linking.openURL(mailUrl).catch(() =>
+      Alert.alert("Error", "Unable to send email.")
+    );
+  };
+
+  const handleChange = (key, value) => {
+    setFormData({ ...formData, [key]: value });
+  };
+
+  const handleSubmit = () => {
+    // Validate form data
+    if (
+      !formData.name ||
+      !formData.empId ||
+      !formData.managerEmail ||
+      !formData.leaveType ||
+      !formData.reason
+    ) {
+      Alert.alert("Error", "Please fill in all fields.");
+      return;
+    }
+    sendEmail();
+  };
+
   return (
-    <View className="flex-1 bg-blue-50">
+    <SafeAreaView className="flex-1 bg-white">
       {/* Header */}
       <View className="flex-row justify-between items-center p-6 bg-blue-700">
         <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
           <Icon name="arrow-back" size={24} className="text-white" />
         </TouchableOpacity>
-        <Text className="text-white text-2xl font-bold">Leave Request</Text>
+        <Text className="text-white text-2xl font-bold">Request Leave</Text>
         <Image source={images.smallLogo} className="w-10 h-10" />
       </View>
 
-      <ScrollView className="p-4">
-        <Text className="text-2xl font-bold text-blue-600 text-center mb-4">
-          Request for Leave
-        </Text>
-
-        {/* Name Fields */}
-        <View className="flex-row mb-4">
-          <TextInput
-            className="flex-1 border p-2 rounded mr-2"
-            placeholder="First Name"
-          />
-          <TextInput
-            className="flex-1 border p-2 rounded"
-            placeholder="Last Name"
-          />
+      <ScrollView className="p-5">
+        {/* Form Fields */}
+        {/* Name Field */}
+        <View className="bg-blue-50 rounded-lg p-4 mb-4">
+          <Text className="text-xl font-bold text-blue-600 mb-2">Name :</Text>
+          <View className="flex-row items-center bg-white rounded-lg">
+            <Icon name="person" className="text-blue-600 mx-4" size={24} />
+            <View className="bg-slate-200 h-[60%] w-px" />
+            <TextInput
+              className="flex-1 p-2 pl-4"
+              placeholder="Enter your name"
+              value={formData.name}
+              onChangeText={(value) => handleChange("name", value)}
+            />
+          </View>
+        </View>
+        {/* Employee ID Field */}
+        <View className="bg-blue-50 rounded-lg p-4 mb-4">
+          <Text className="text-xl font-bold text-blue-600 mb-2">
+            Employee ID :
+          </Text>
+          <View className="flex-row items-center bg-white rounded-lg">
+            <Icon
+              name="account-circle"
+              className="text-blue-600 mx-4"
+              size={24}
+            />
+            <View className="bg-slate-200 h-[60%] w-px" />
+            <TextInput
+              className="flex-1 p-2 pl-4"
+              placeholder="Enter your employee ID"
+              value={formData.empId}
+              onChangeText={(value) => handleChange("empId", value)}
+            />
+          </View>
+        </View>
+        {/* Phone Number Field */}
+        <View className="bg-blue-50 rounded-lg p-4 mb-4">
+          <Text className="text-xl font-bold text-blue-600 mb-2">
+            Phone Number :
+          </Text>
+          <View className="flex-row items-center bg-white rounded-lg">
+            <Icon name="phone" className="text-blue-600 mx-4" size={24} />
+            <View className="bg-slate-200 h-[60%] w-px" />
+            <TextInput
+              className="flex-1 p-2 pl-4"
+              placeholder="Enter your phone number"
+              value={formData.phoneNumber}
+              onChangeText={(value) => handleChange("phoneNumber", value)}
+              keyboardType="phone-pad"
+            />
+          </View>
+        </View>
+        {/* Manager Name Field */}
+        <View className="bg-blue-50 rounded-lg p-4 mb-4">
+          <Text className="text-xl font-bold text-blue-600 mb-2">
+            Manager Name :
+          </Text>
+          <View className="flex-row items-center bg-white rounded-lg">
+            <Icon name="person" className="text-blue-600 mx-4" size={24} />
+            <View className="bg-slate-200 h-[60%] w-px" />
+            <TextInput
+              className="flex-1 p-2 pl-4"
+              placeholder="Enter your manager's name"
+              value={formData.managerName}
+              onChangeText={(value) => handleChange("managerName", value)}
+            />
+          </View>
+        </View>
+        {/* Manager Email Field */}
+        <View className="bg-blue-50 rounded-lg p-4 mb-4">
+          <Text className="text-xl font-bold text-blue-600 mb-2">
+            Manager Email :
+          </Text>
+          <View className="flex-row items-center bg-white rounded-lg">
+            <Icon name="email" className="text-blue-600 mx-4" size={24} />
+            <View className="bg-slate-200 h-[60%] w-px" />
+            <TextInput
+              className="flex-1 p-2 pl-4"
+              placeholder="Enter your manager's email"
+              value={formData.managerEmail}
+              onChangeText={(value) => handleChange("managerEmail", value)}
+              keyboardType="email-address"
+            />
+          </View>
+        </View>
+        <View className="bg-blue-50 rounded-lg p-4 mb-4">
+          <Text className="text-xl font-bold text-blue-600 mb-2">
+            Leave Type :
+          </Text>
+          <View className="bg-white rounded-lg">
+            <Picker
+              selectedValue={formData.leaveType}
+              onValueChange={(itemValue) =>
+                handleChange("leaveType", itemValue)
+              }
+            >
+              <Picker.Item label="Select leave type" value="" />
+              <Picker.Item label="Annual Leave" value="annual" />
+              <Picker.Item label="Sick Leave" value="sick" />
+              <Picker.Item label="Personal Leave" value="personal" />
+              <Picker.Item label="Other Reason" value="other" />
+            </Picker>
+          </View>
         </View>
 
-        {/* Employee ID */}
-        <TextInput
-          className="border p-2 rounded mb-4"
-          placeholder="Employee ID"
-          keyboardType="numeric"
-        />
+        {/* Date Section */}
+        <View className="bg-blue-50 rounded-lg p-4 mb-4">
+          <View className="flex-row items-center justify-between">
+            {/* Start Date Section */}
+            <View className="flex-1 mr-2">
+              <Text className="text-xl font-bold text-blue-600 mb-2">
+                Start Date:
+              </Text>
+              <View className="flex-row items-center bg-white rounded-lg">
+                <Icon
+                  name="calendar-today"
+                  className="text-blue-600 mx-2"
+                  size={24}
+                />
+                <TouchableOpacity
+                  onPress={showStartDatePicker}
+                  className="bg-white p-3 rounded-lg"
+                >
+                  <Text>{startDate.toLocaleDateString()}</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
 
-        {/* Email */}
-        <TextInput
-          className="border p-2 rounded mb-4"
-          placeholder="Email"
-          keyboardType="email-address"
-        />
-
-        {/* Phone Number */}
-        <TextInput
-          className="border p-2 rounded mb-4"
-          placeholder="Phone Number"
-          keyboardType="phone-pad"
-        />
-
-        {/* Position */}
-        <TextInput className="border p-2 rounded mb-4" placeholder="Position" />
-
-        {/* Manager and Manager Email */}
-        <TextInput
-          className="border p-2 rounded mb-4"
-          placeholder="Manager Name"
-        />
-        <TextInput
-          className="border p-2 rounded mb-4"
-          placeholder="Manager Email"
-          keyboardType="email-address"
-        />
-
-        {/* Leave Request For */}
-        <Text className="text-lg font-semibold mb-2">Details of Leave</Text>
-        <View className="flex-row items-center mb-4">
-          <TouchableOpacity className="mr-4">
-            <Text className="text-blue-600">Days</Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Hours</Text>
-          </TouchableOpacity>
+            {/* End Date Section */}
+            <View className="flex-1">
+              <Text className="text-xl font-bold text-blue-600 mb-2">
+                End Date:
+              </Text>
+              <View className="flex-row items-center bg-white rounded-lg">
+                <Icon
+                  name="calendar-today"
+                  className="text-blue-600 mx-2"
+                  size={24}
+                />
+                <TouchableOpacity
+                  onPress={showEndDatePicker}
+                  className="bg-white p-3 rounded-lg"
+                >
+                  <Text>{endDate.toLocaleDateString()}</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
         </View>
+        {/* Reason for Leave */}
+        <View className="bg-blue-50 rounded-lg p-4 mb-4">
+          <Text className="text-xl font-bold text-blue-600 mb-2">
+            Reason for Leave:
+          </Text>
 
-        {/* Leave Start and End Dates */}
-        <View className="flex-row mb-4">
-          <TouchableOpacity
-            className="flex-1 border p-2 rounded mr-2"
-            onPress={showStartDatePicker}
-          >
-            <Text>{startDate.toLocaleDateString()}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="flex-1 border p-2 rounded"
-            onPress={showEndDatePicker}
-          >
-            <Text>{endDate.toLocaleDateString()}</Text>
-          </TouchableOpacity>
+          <View className="bg-white flex-row rounded-lg p-2">
+            <Icon
+              name="text-snippet"
+              size={24}
+              className="text-blue-600 mx-2 self-start"
+            />
+            <TextInput
+              className="flex-1 pl-2"
+              placeholder="Please specify your reason for leave"
+              value={formData.reason}
+              onChangeText={(value) => handleChange("reason", value)}
+              multiline={true}
+              numberOfLines={5}
+              textAlignVertical="top"
+              style={{ maxHeight: 100 }}
+            />
+          </View>
         </View>
-
-        {/* Leave Type */}
-        <Picker
-          selectedValue={leaveType}
-          onValueChange={(itemValue) => setLeaveType(itemValue)}
-          className="border p-2 rounded mb-4"
-        >
-          <Picker.Item label="Vacation" value="Vacation" />
-          <Picker.Item label="Sick" value="Sick" />
-          <Picker.Item label="Quitting" value="Quitting" />
-          <Picker.Item label="Other" value="Other" />
-        </Picker>
-
-        {/* Comments */}
-        <TextInput
-          className="border p-2 rounded mb-4"
-          placeholder="Comments"
-          multiline
-          numberOfLines={4}
-        />
 
         {/* Submit Button */}
-        <TouchableOpacity className="bg-blue-600 p-4 rounded">
-          <Text className="text-center text-white font-semibold">
-            Request Leave
+        <TouchableOpacity
+          onPress={handleSubmit}
+          className="w-full bg-blue-600 text-white rounded-lg py-3 flex-row justify-center items-center mb-10"
+        >
+          <Icon name="send" size={18} color="white" className="mr-2" />
+          <Text className="text-white text-center text-lg font-bold">
+            Submit Leave Request
           </Text>
         </TouchableOpacity>
       </ScrollView>
-    </View>
+    </SafeAreaView>
   );
 };
 
